ac340570f148f982278879bb7241ec25724dc45f
KhoiC <khoi.cao1@education.nsw.gov.au>
2025-05-21 01:55:09 +0000

    Added Cards

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route('/index.html')
 @app.route('/')
 def index():
-    return render_template("index.html"), 200
+    card_data = (
+        ("Features Of Laptops", "Learn about the features laptops have to offer", "URL", "static/images/image-url"),
+        ("Features Of Desktops", "Learn about the features desktops have to offer", "URL", "static/images/image-url"),
+        ("Examples Of Laptps", "Find example laptops", "URL", "static/images/image-url"),
+    )
+    return render_template("index.html", cards = card_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
