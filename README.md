78350a78bc30eff4ac34a6b5a5e5a235fbfb07ee
KhoiC <khoi.cao1@education.nsw.gov.au>
2025-06-03 01:55:21 +0000

    changed logo color

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ change
 git add -A
 git commit -m "add readme"
 git push
+
+To Do:
+
+1. Create graphics + images
+2. Add to the home page
+3. Work on other pages
+4.
+5.
--- main.py
+++ main.py
@@ -7,11 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Features Of Laptops", "Learn about the features laptops have to offer", "URL", "static/images/image-url"),
-        ("Features Of Desktops", "Learn about the features desktops have to offer", "URL", "static/images/image-url"),
-        ("Examples Of Laptps", "Find example laptops", "URL", "static/images/image-url"),
+        ("Features Of Laptops", "Learn about the features laptops have to offer", "Learn More", "static/images/Stuff/1.png"),
+        ("Features Of Desktops", "Learn about the features desktops have to offer", "Learn More", "static/images/Stuff/2.png"),
+        ("Examples Of Laptops", "Find example laptops", "Learn More", "static/images/Stuff/3.png"),
+        ("Examples Of Desktops", "Find example desktops", "Learn More", "static/images/Stuff/4.png"),
+        ("Which Is Better (For Students)", "Determine which is better for school", "Learn More", "static/images/Stuff/5.png"),
+        ("Which Is Better (For Students)", "Determine which is better for the workplace", "Learn More", "static/images/Stuff/6.png"),
     )
     return render_template("index.html", cards = card_data), 200
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
