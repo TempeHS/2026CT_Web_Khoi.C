f4f86d5acb2e40551a86a57b35f57b0ae6a034c3
KhoiC <khoi.cao1@education.nsw.gov.au>
2025-06-04 01:55:31 +0000

    added login page #1

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Features Of Laptops", "Learn about the features laptops have to offer", "Learn More", "static/images/Stuff/1.png"),
-        ("Features Of Desktops", "Learn about the features desktops have to offer", "Learn More", "static/images/Stuff/2.png"),
-        ("Examples Of Laptops", "Find example laptops", "Learn More", "static/images/Stuff/3.png"),
-        ("Examples Of Desktops", "Find example desktops", "Learn More", "static/images/Stuff/4.png"),
+        ("About Laptops", "Learn about the features laptops have to offer", "Learn More", "static/images/Stuff/1.png"),
+        ("About Desktops", "Learn about the features desktops have to offer", "Learn More", "static/images/Stuff/2.png"),
+        ("Quality Laptops", "Find example laptops", "Learn More", "static/images/Stuff/3.png"),
+        ("Quality Desktops", "Find example desktops", "Learn More", "static/images/Stuff/4.png"),
         ("Which Is Better (For Students)", "Determine which is better for school", "Learn More", "static/images/Stuff/5.png"),
         ("Which Is Better (For Students)", "Determine which is better for the workplace", "Learn More", "static/images/Stuff/6.png"),
     )
@@ -19,6 +19,10 @@ def index():
 @app.route('/contact.html')
 def contact():
     return render_template("contact.html"), 200
+
+@app.route('/login.html')
+def login():
+    return render_template("login.html"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
