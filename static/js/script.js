74156b632d62ad2be91d9ab293a91ffd38becdc4
KhoiC <khoi.cao1@education.nsw.gov.au>
2025-07-02 01:55:04 +0000

    added show password

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,22 @@
+function checkFormContact() {
+  const name = document.getElementById("contactName1");
+  const email = document.getElementById("contactEmail1");
+  const phone = document.getElementById("contactPhone1");
+  if (!name.value) {
+    showAlert("Please enter a name.", "danger");
+    return;
+  }
+  if (!email.value) {
+    showAlert("Please enter an email.", "danger");
+    return;
+  }
+  if (!phone.value) {
+    showAlert("Please enter a phone number.", "danger");
+    return;
+  }
+  showAlert("You sent a message!", "primary");
+}
+
 function showAlert(message, type) {
   const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
   const wrapper = document.createElement("div");
@@ -9,11 +28,43 @@ function showAlert(message, type) {
   ].join("");
 
   alertPlaceholder.append(wrapper);
+
+  setTimeout(() => {
+    wrapper.remove();
+  }, 3000);
 }
 
-const alertTrigger = document.getElementById("liveAlertBtn");
-if (alertTrigger) {
-  alertTrigger.addEventListener("click", () => {
-    appendAlert("You sent a message!", "primary");
-  });
+function checkFormLogin() {
+  const email = document.getElementById("loginEmail1");
+  const password = document.getElementById("loginPassword1");
+  if (!email.value) {
+    showAlert("Please enter an email.", "danger");
+    return;
+  }
+  if (!password.value) {
+    showAlert("Please enter an password.", "danger");
+    return;
+  }
+  window.location.href = "/index.html";
+}
+
+function togglePassword(passwordId) {
+  const password = document.getElementById(passwordId);
+  if (password.type == "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
+
+function toggleDoublePassword(passwordId1, passwordId2) {
+  const p1 = document.getElementById(passwordId1);
+  const p2 = document.getElementById(passwordId2);
+  if (p1.type == "password") {
+    p1.type = "text";
+    p2.type = "text";
+  } else {
+    p1.type = "password";
+    p2.type = "password";
+  }
 }
